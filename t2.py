c7015f4f456b6d4a520857fd798bcab345762ba1
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2017-08-28 16:35:14 -0300

    creates a dictionary of date>headline from the csv
    (really unsure if this is a decent way to split the content, but python still seems alien. lets see how this goes...)

--- t2.py
+++ t2.py
@@ -1,12 +1,24 @@
+headlines = {}
+
 def create_headline_list(filename):
   f = open(filename,'rU')
   text = f.readlines()
+  for lines in text:
+    values = lines.split(',',4)
+    data = (values[2],values[1][1:-1],values[0])
+    headline = (values[3][1:-1],values[4][1:-2])
+    print(headline)
+    if data not in headlines:
+      headlines[data] = []
+    headlines[data].append(headline)
+  print(headlines)
+
   return text
   f.close()
 
 def main():
   text = create_headline_list('manchetesBrasildatabase/manchetesBrasildatabase.csv')
-  print(text)
+  # print(text)
 
 
 if __name__ == '__main__':
